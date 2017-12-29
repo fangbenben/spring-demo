eb43f660fdff7fb83882ec1f97a7da23b96a761e
fangbenben <136581240@qq.com>
2017-12-29 13:05:25 +0800

    Initial commit

---
+++ src/test/java/com/fang/pojo/PersonTest.java
@@ -0,0 +1,13 @@
+package com.fang.pojo;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/12/28 0028.
+ */
+public class PersonTest {
+    @Test
+    public void test1(){
+        System.out.print("aaaaaaa");
+    }
+}
